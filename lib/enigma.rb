299a0b277e88655433122523b0f1fcf62b79c981
Tyler Ross <tross020890@gmail.com>
2022-01-14 23:18:00 -0600

    Feat: gets keys

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,15 @@
+#encrypts and decrypts messages
+#require './encrypt'
 class Enigma
+
+  def get_keys(key)
+    keys = [0, 0, 0, 0]
+    keys.each_with_index do |num, index|
+      keys[index] = (10*(key[index].to_i))+(key[index+1].to_i)
+    end
+    require 'pry'; binding.pry
+  end
+
+
 
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,7 +1,30 @@
 require 'simplecov'
 SimpleCov.start
 require './lib/enigma'
+require 'date'
 
 RSpec.describe Enigma do
+  it "exists" do
+    enigma = Enigma.new
+
+    expect(enigma).to be_instance_of(Enigma)
+  end
+
+  it "can get keys" do
+    enigma = Enigma.new
+
+    enigma.get_keys("02715")
+
+    expect(keys).to be_instance_of(Array)
+  end
+
+  it "gets keys" do
+    enigma = Enigma.new
+
+    enigma.get_keys("02715")
+
+    expect(keys).to eq([2, 27, 71, 15])
+  end
+
 
 end
