5d65287844131b122deffaa2e4ed982100d32a56
Tyler Ross <tross020890@gmail.com>
2022-01-15 00:04:29 -0600

    Feat: gets offset from date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,9 +7,16 @@ class Enigma
     keys.each_with_index do |num, index|
       keys[index] = (10*(key[index].to_i))+(key[index+1].to_i)
     end
-    require 'pry'; binding.pry
+    return keys
   end
 
-
-
+  def get_offset(date)
+    square_date = (date.to_i*date.to_i).to_s
+    square_date = square_date.split("")
+    while square_date.length > 4 do
+      square_date.delete_at(0)
+    end
+    square_date.map {|num| num.to_i}
+  end
+  #require 'pry'; binding.pry
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe Enigma do
   it "can get keys" do
     enigma = Enigma.new
 
-    enigma.get_keys("02715")
 
-    expect(keys).to be_instance_of(Array)
+
+    expect(enigma.get_keys("02715")).to be_instance_of(Array)
   end
 
   it "gets keys" do
@@ -23,7 +23,22 @@ RSpec.describe Enigma do
 
     enigma.get_keys("02715")
 
-    expect(keys).to eq([2, 27, 71, 15])
+    expect(enigma.get_keys("02715")).to eq([2, 27, 71, 15])
+  end
+
+  it "can get offset" do
+    enigma = Enigma.new
+
+    enigma.get_offset("040895")
+
+    expect(enigma.get_offset("040895")).to be_instance_of(Array)
+  end
+  it "gets offset" do
+    enigma = Enigma.new
+
+    enigma.get_offset("040895")
+
+    expect(enigma.get_offset("040895")).to eq([1, 0, 2, 5])
   end
 
 
