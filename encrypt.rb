e7d5e1db422c9a01d52f01aad97bd7366f4abb2b
Tyler Ross <tross020890@gmail.com>
2022-01-17 18:03:03 -0600

    Feat: runner decrypts from/to files

--- encrypt.rb
+++ encrypt.rb
@@ -6,14 +6,12 @@ enigma = Enigma.new
 @encrypted_data = {}
 
 File.open(split_files.first, "r") do |file|
-  message = file.readline()
-
+  message = file.readline().chomp
   @encrypted_data=enigma.encrypt(message)
 end
 
- File.open(split_files.last, "a") do |file|
-  # require 'pry'; binding.pry
-   file.write("#{@encrypted_data[:encryption]} #{@encrypted_data[:key]} #{@encrypted_data[:date]} ")
-
+ File.open(split_files.last, "w") do |file|
+   #require 'pry'; binding.pry
+   file.write("#{@encrypted_data[:encryption].chomp} #{@encrypted_data[:key]} #{@encrypted_data[:date]} ")
    puts "Created #{split_files.last} with the key #{@encrypted_data[:key]} and date #{@encrypted_data[:date]}"
  end
--- lib/shifter.rb
+++ lib/shifter.rb
@@ -1,7 +1,8 @@
+require 'date'
 module Shifter
 
   def get_keys(key)
-    key = valid_key(key)
+    #key = valid_key(key)
     keys = [0, 0, 0, 0]
     keys.each_with_index do |num, index|
       keys[index] = (10*(key[index].to_i))+(key[index+1].to_i)
@@ -10,7 +11,7 @@ module Shifter
   end
 
   def get_offset(date)
-    date = valid_date(date)
+    #date = valid_date(date)
     square_date = (date.to_i*date.to_i).to_s
     square_date = square_date.split("")
     while square_date.length > 4 do
